0a893508e9b3eb7aeaeac3f74d66f05f2ffe3bcf
gmsdev04 <gmsdev04@icloud.com>
2023-07-08 00:38:20 -0300

    incluindo ProductServiceImpl

---
+++ src/main/kotlin/com/github/gmsdev04/productapi/service/impl/ProductServiceImpl.kt
@@ -0,0 +1,14 @@
+package com.github.gmsdev04.productapi.service.impl
+
+import com.github.gmsdev04.productapi.domain.Product
+import com.github.gmsdev04.productapi.service.ProductService
+import org.springframework.stereotype.Service
+
+@Service
+class ProductServiceImpl : ProductService {
+    override fun create(product: Product): Product {
+        TODO("Not yet implemented")
+    }
+
+
+}
